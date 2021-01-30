0efa9a0399f835dde56f70640d9336987a8218c9
GuyNa <34960387+guyna25@users.noreply.github.com>
2021-01-30 15:09:33 +0200

    agent shape well, problem with tensor

--- testing.py
+++ testing.py
@@ -33,25 +33,19 @@ import matplotlib.pyplot as plt
 from pettingzoo.mpe import simple_speaker_listener_v3
 # register_env("TaxiMultiAgentEnv", TaxiEnv)
 
-def env_creator(config):
-    env = simple_speaker_listener_v3.env()
-    # env = dtype_v0(env, dtype=float32)
-    # env = color_reduction_v0(env, mode="R")
-    # env = normalize_obs_v0(env)
-    return env
 alg_name = "PPO"
 #config = deepcopy(get_agent_class(alg_name)._default_config)
 
-#config = deepcopy(a2c.A2C_DEFAULT_CONFIG.copy())
+config = deepcopy(a2c.A2C_DEFAULT_CONFIG)
 # config["env"] = "simple_speaker_listner"
-# config["env_config"] = None
-# config["rollout_fragment_length"] = 20
-# config["num_workers"] = 5
-# config["num_envs_per_worker"] = 1
-# config["lr_schedule"] = [[0, 0.007], [20000000, 0.0000000001]]
-#
-# config["callbacks"] = MyCallbacks
-# config["clip_rewards"] = True
+config["env_config"] = None
+config["rollout_fragment_length"] = 20
+config["num_workers"] = 5
+config["num_envs_per_worker"] = 1
+config["lr_schedule"] = [[0, 0.007], [20000000, 0.0000000001]]
+
+#config["callbacks"] = MyCallbacks
+config["clip_rewards"] = True
 s = "{:3d} reward {:6.2f}/{:6.2f}/{:6.2f} len {:6.2f}"
 multiagent_dict = dict()
 multiagent_policies = dict()
@@ -63,15 +57,17 @@ agents_name = deepcopy(env.possible_agents)
 config = {
           "num_gpus": 0,
           "num_workers": 1,
-          #"env": SLenv
           }
-register_env("simple_speaker_listner", lambda config: PettingZooEnv(env_creator({})))
+env = simple_speaker_listener_v3.env()
+#mod_env = supersuit.aec_wrappers.pad_action_space(env)
+#mod_env = supersuit.aec_wrappers.pad_observations(mod_env)
+mod_env = supersuit.aec_wrappers.pad_action_space(env)
+mod_env = supersuit.aec_wrappers.pad_observations(mod_env)
+mod_env = PettingZooEnv(mod_env)
+register_env("simple_speaker_listener", lambda stam: mod_env)
 
-test_env = supersuit.aec_wrappers.pad_observations(PettingZooEnv(env_creator({})))
-obs_space = test_env.observation_space
-act_space = test_env.action_space
 ray.init(num_gpus=0, local_mode=True)
-agent = a2c.A2CTrainer(env="simple_speaker_listener")
+agent = a2c.A2CTrainer(env="simple_speaker_listener", config=config)
 
 for it in range(5):
     result = agent.train()
@@ -82,4 +78,4 @@ for it in range(5):
         result["episode_reward_max"],
         result["episode_len_mean"]
     ))
-    test_env.reset()
+    mod_env.reset()
